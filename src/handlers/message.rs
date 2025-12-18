e5e6edf3ff3ed4d7fbdaba5772eecb22f564bc8d
k1p1k-code <relax.1ngkl@gmail.com>
2025-12-18 17:18:57 +0300

    Update system plugins

--- src/handlers/message.rs
+++ src/handlers/message.rs
@@ -9,8 +9,6 @@ use crate::plugins_py::Plugin;
 use crate::plugins_py::utils::run_hook;
 use crate::utils::*;
 
-
-
 pub async fn message_handler(message: Message, sender: &FunPaySender, me: &FPMe, strategies: &Strategies, plugin: &Vec<Plugin>) {
     let message_for_plugins=Arc::new(
         message_to_json_value(&message).to_string()
@@ -18,12 +16,17 @@ pub async fn message_handler(message: Message, sender: &FunPaySender, me: &FPMe,
     let me_for_plugins=Arc::new(
         funpay_me_to_json_value(&me).to_string()
     );
+    let args_py = (
+        message_for_plugins.clone(),
+        me_for_plugins.clone()
+    );
     for i in plugin.iter() {
-        let args_py = (
-            message_for_plugins.clone(),
-            me_for_plugins.clone()
-            );
-        let con=match run_hook(&i.message_hook, args_py).await {
+        let args_py=args_py.clone();
+        let message_hook=match &i.message_hook { 
+            Some(hook) => {hook},
+            None => {continue;}
+        };
+        let con=match run_hook(message_hook, args_py).await {
             Ok(b) => b,
             Err(e) => {println!("Plugin \"{}\" message hook returned error, about message_handler!\nError: {:?}", i.name, e); return;},
         };
@@ -31,11 +34,7 @@ pub async fn message_handler(message: Message, sender: &FunPaySender, me: &FPMe,
             return;
         }
     }
-
-
-
-
-
+    
     if me.id == message.author_id{return;}
 
     let text=match message.text {
