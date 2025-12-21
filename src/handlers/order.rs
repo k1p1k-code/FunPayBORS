a1a0c5b5b49fdd622cd5904924f8ebb7658b92df
k1p1k-code <relax.1ngkl@gmail.com>
2025-12-21 18:42:21 +0300

    fix

--- src/handlers/order.rs
+++ src/handlers/order.rs
@@ -1,5 +1,6 @@
+use std::process::exit;
 use std::sync::Arc;
-use funpay_client::FunPaySender;
+use funpay_client::{FunPayError, FunPaySender};
 use funpay_client::models::{OrderShortcut};
 use crate::models::FPMe;
 use crate::models::strategy::Strategies;
@@ -24,7 +25,7 @@ pub async fn order_handler(order: OrderShortcut, sender: &FunPaySender, me: &FPM
             Some(hook) => {hook},
             None => {continue;}
         };
-        let con=match run_hook(order_hook, args_py).await {
+        let con=match run_hook(order_hook, Some(args_py)).await {
             Ok(b) => b,
             Err(e) => {println!("Plugin \"{}\" message hook returned error, about message_handler!\nError: {:?}", i.name, e); return;},
         };
@@ -35,10 +36,20 @@ pub async fn order_handler(order: OrderShortcut, sender: &FunPaySender, me: &FPM
     for i in &strategies.order{
         if  order.description.starts_with(i.unique_prefix.as_str()){
             if let Some(static_data) = &i.static_data {
-                sender.send_chat_message(&order.chat_id, static_data.as_str()).await.unwrap();
+                sender.send_chat_message(&order.chat_id, static_data.as_str()).await.unwrap_or_else(|m|{
+                    match m {
+                        FunPayError::Unauthorized => { println!("The golden Key became invalid, exit");exit(401)},
+                        _ => {println!("Error send message {:?}", m);}
+                    }
+                })
             }
             if let Some(availability) = &i.get_availability() {
-                sender.send_chat_message(&order.chat_id, availability.as_str()).await.unwrap();
+                sender.send_chat_message(&order.chat_id, availability.as_str()).await.unwrap_or_else(|m|{
+                    match m {
+                        FunPayError::Unauthorized => { println!("The golden Key became invalid, exit");exit(401)},
+                        _ => {println!("Error send message {:?}", m);}
+                    }
+                })
             }
             break
         }
