e718aa1f741d85ceb80ec2adf9acca14a9125d60
k1p1k-code <k1p1k-code@github.com>
2025-12-25 23:02:32 +0300

    Add #[allow(deprecated)] in line with downcast

--- src/plugins_py/mod.rs
+++ src/plugins_py/mod.rs
@@ -1,11 +1,10 @@
 pub mod utils;
 
-use std::ffi::CString;
 use pyo3::prelude::*;
+use pyo3::types::PyDict;
+use std::ffi::CString;
 use std::fs;
 use std::path::PathBuf;
-use pyo3::types::PyDict;
-
 
 pub struct Plugin {
     #[warn(unused_variables)]
@@ -13,7 +12,6 @@ pub struct Plugin {
     pub message_hook: Option<Py<PyAny>>,
     pub order_hook: Option<Py<PyAny>>,
 }
-
 
 #[derive(Debug)]
 pub struct InfoPlugin {
@@ -23,47 +21,58 @@ pub struct InfoPlugin {
 
 fn extruct_plugin(info_plugin: InfoPlugin) -> Plugin {
     Python::attach(|py| {
-        let plugin_dir = info_plugin.path_plugin.parent()
+        let plugin_dir = info_plugin
+            .path_plugin
+            .parent()
             .expect("Failed to get plugin directory");
         let sys = py.import("sys").unwrap();
         let binding = sys.getattr("path").unwrap();
-        let path=binding.downcast().unwrap();
+        #[allow(deprecated)]
+        let path = binding.downcast().unwrap();
+
         let plugin_dir_str = plugin_dir.to_str().unwrap();
         path.insert(0, plugin_dir_str).unwrap();
 
         let locals = PyDict::new(py);
 
-        let plugin_code = fs::read_to_string(&info_plugin.path_plugin)
-            .expect(&format!("Failed to read plugin: {:?}", info_plugin.path_plugin));
+        let plugin_code = fs::read_to_string(&info_plugin.path_plugin).expect(&format!(
+            "Failed to read plugin: {:?}",
+            info_plugin.path_plugin
+        ));
 
-        let c_plugin_code = CString::new(plugin_code)
-            .expect(&format!("Failed to convert plugin code to CString: {:?}", info_plugin.path_plugin));
+        let c_plugin_code = CString::new(plugin_code).expect(&format!(
+            "Failed to convert plugin code to CString: {:?}",
+            info_plugin.path_plugin
+        ));
 
-        py.run(&c_plugin_code, None, Some(&locals))
-            .expect(&format!("Failed to run plugin: {:?}", info_plugin.path_plugin));
+        py.run(&c_plugin_code, None, Some(&locals)).expect(&format!(
+            "Failed to run plugin: {:?}",
+            info_plugin.path_plugin
+        ));
 
-
-        let plugin_class = locals.get_item("Plugin")
+        let plugin_class = locals
+            .get_item("Plugin")
             .expect("No \"Plugin\" class found")
             .expect("No \"Plugin\" class found.");
 
-        let plugin_instance = plugin_class.call0()
+        let plugin_instance = plugin_class
+            .call0()
             .expect("Failed to create Plugin instance");
 
-        let message_hook: Option<Py<PyAny>> = match plugin_instance.getattr("message_hook"){
+        let message_hook: Option<Py<PyAny>> = match plugin_instance.getattr("message_hook") {
             Ok(hook) => Some(hook.into()),
-            Err(_) => None
+            Err(_) => None,
         };
 
-        let order_hook: Option<Py<PyAny>> = match plugin_instance.getattr("order_hook"){
+        let order_hook: Option<Py<PyAny>> = match plugin_instance.getattr("order_hook") {
             Ok(hook) => Some(hook.into()),
-            Err(_) => None
+            Err(_) => None,
         };
 
-        let load = plugin_instance.getattr("load")
+        let load = plugin_instance
+            .getattr("load")
             .expect("No \"load\" method in Plugin found");
-        load.call0()
-            .expect("Failed to call load method");
+        load.call0().expect("Failed to call load method");
 
         Plugin {
             name: info_plugin.name,
@@ -76,20 +85,21 @@ fn extruct_plugin(info_plugin: InfoPlugin) -> Plugin {
 pub fn loader_plugins() -> Result<Vec<Plugin>, String> {
     let current_dir = std::env::current_dir().unwrap();
     let path_plugins = current_dir.join("plugins");
-    let path_venv=path_plugins.join("venv");
-    unsafe { std::env::set_var("VIRTUAL_ENV", &path_venv); }
+    let path_venv = path_plugins.join("venv");
+    unsafe {
+        std::env::set_var("VIRTUAL_ENV", &path_venv);
+    }
     let mut plugins = Vec::new();
     if !path_plugins.exists() {
         Err("\"/plugins\" directory does not exist.".to_string())?
     }
 
-    if !path_venv.exists(){
+    if !path_venv.exists() {
         Err("Warning: \"/plugins/venv\" directory does not exist, use global interpreter.")?
     }
 
-    let entries = fs::read_dir(&path_plugins)
-        .expect("Failed to read plugins directory");
-    
+    let entries = fs::read_dir(&path_plugins).expect("Failed to read plugins directory");
+
     for entry in entries {
         let entry = entry.expect("Failed to read directory entry");
         if entry.path().is_dir() && entry.file_name() != "venv" {
@@ -100,7 +110,10 @@ pub fn loader_plugins() -> Result<Vec<Plugin>, String> {
             if !path_plugin.exists() {
                 Err(format!("Plugin file not exist {:?}", path_plugin))?
             }
-            let plugin = extruct_plugin(InfoPlugin {path_plugin: entry.path().join("plugin.py"), name: plugin_name});
+            let plugin = extruct_plugin(InfoPlugin {
+                path_plugin: entry.path().join("plugin.py"),
+                name: plugin_name,
+            });
             plugins.push(plugin);
         }
     }
