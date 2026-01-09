fb45f65354c9fca99998a449d69bf4d73043ebd8
k1p1k-code <hopkopgo@gmail.com>
2026-01-09 23:27:59 +0300

    !Global update | add static!

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -10,29 +10,33 @@ use axum::{
     routing::{post, get},
     http::Method,
 };
-use tower_http::cors::{CorsLayer, Any};
+use tower_http::{
+    cors::{CorsLayer, Any},
+    services::ServeDir,
+};
 use tower::ServiceBuilder;
-use plugins::{ list_plugins, reload_plugins, callback_plugin};
 
+use plugins::{ list_plugins, reload_plugins, callback_plugin};
 pub async fn build_router(app_state: Arc<Mutex<AppState>>) -> Router {
     let cors_layer = CorsLayer::new()
-        .allow_origin(Any)  // Open access to selected route
-        .allow_headers(Any)  // ← Разрешаем ВСЕ заголовки
+        .allow_origin(Any)
+        .allow_headers(Any)
         .allow_methods([
             Method::GET,
             Method::POST,
             Method::PUT,
             Method::DELETE,
-            Method::OPTIONS,  // Важно для CORS preflight
+            Method::OPTIONS,
         ]);
 
+    let static_files = ServeDir::new("html")
+        .append_index_html_on_directories(true);
 
-    let app = Router::new()
+    Router::new()
         .route("/plugins/callback", post(callback_plugin))
         .route("/plugins/list", get(list_plugins))
         .route("/plugins/reload", post(reload_plugins))
+        .fallback_service(static_files)
         .with_state(app_state)
-        .layer(ServiceBuilder::new().layer(cors_layer));
-
-    app
+        .layer(ServiceBuilder::new().layer(cors_layer))
 }
